adcf07743cac2f5a570c029daecad4484d9c8702
joeypiccola <joey.piccola@gmail.com>
2016-12-16 20:10:50 -0700

    remove dva

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
     packageName    = $packageName
-    url            = 'https://artifactory.davita.com/artifactory/win-binaries/FramePkg.exe'
+    url            = 'https://artifactory.-.com/artifactory/win-binaries/FramePkg.exe'
     checksum       = '4E1731F987FDA4B42E73EB9E03AB9AB944E4A72A29E4D3BB9D07B87BBE14648C'
     checksumtype   = 'sha256' 
     fileType       = 'exe'
